c11969eefedf11c0984a000b3dfb68e72510e5e2
wachira140 <wachiraw424@gmail>
2023-01-05 05:55:57 -0500

    spelling err

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 USE hbnb_dev_db;
-GRANT ALL PRIVELEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
