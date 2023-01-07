5d98d7b06e6894ebd54c2b8047141d0216c63df4
wachira140 <wachiraw424@gmail>
2023-01-07 13:14:43 -0500

    db bug fixing

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,9 +2,8 @@
 --GRANT *.* to user hbnb_test
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-USE hbnb_dev_db;
+
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-USE performance_schema;
+
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
-SET FOREIGN_KEY_CHECKS=1;
