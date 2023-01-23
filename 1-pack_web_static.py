c62a8a0068b7019a222018a65e046e1d261ffc23
wachira141 <wachiraw75@gmail.com>
2023-01-23 05:13:20 -0800

    compress file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-'''Fabric script to generate .tgz archive'''
-
-from fabric.api import local
+"""A module for Fabric script that generates a .tgz archive."""
+import os
 from datetime import datetime
-
-from fabric.decorators import runs_once
+from fabric.api import local, runs_once
 
 
 @runs_once
 def do_pack():
-    '''generates .tgz archive from the contents of the web_static folder'''
-    local("mkdir -p versions")
-    path = ("versions/web_static_{}.tgz"
-            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
-    result = local("tar -cvzf {} web_static"
-                   .format(path))
-
-    if result.failed:
-        return None
-    return path
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    d_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        d_time.year,
+        d_time.month,
+        d_time.day,
+        d_time.hour,
+        d_time.minute,
+        d_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, size))
+    except Exception:
+        output = None
+    return output
